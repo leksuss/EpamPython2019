b70c399e117491f2569231c894e7e16317d563d7
Andrey <leksus@leksus.info>
2019-11-25 22:19:14 +0300

    reduce changed to map in 03-fp-decorator hw2

--- 03-fp-decorator/hw/hw2.py
+++ 03-fp-decorator/hw/hw2.py
@@ -1,11 +1,7 @@
-from functools import reduce
-
-
 def is_armstrong(number):
-    return int(reduce(lambda x, y: int(x) +
-                      int(y)**len(str(number)), list(str(number)))) == number
+    return sum(map(lambda x: int(x)**len(str(number)),
+                   list(str(number)))) == number
 
 
 assert is_armstrong(153) == True, 'Число Армстронга'
 assert is_armstrong(10) == False, 'Не число Армстронга'
-
