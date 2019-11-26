9f1bd581fd0566514ed66851a88b8b59865a6fba
Andrey <leksus@leksus.info>
2019-11-26 09:39:55 +0300

    fixed 03-fp-decorator hw3

--- 03-fp-decorator/hw/hw3.py
+++ 03-fp-decorator/hw/hw3.py
@@ -1,11 +1,12 @@
-
 def collatz_steps(n):
     def inner(n, steps):
-        return steps if n == 1 else inner(
+        return steps if n <= 1 else inner(
             3 * n + 1 if n % 2 else n / 2, steps + 1)
     return inner(n, 0)
 
 
+assert collatz_steps(0) == 0
+assert collatz_steps(1) == 0
 assert collatz_steps(16) == 4
 assert collatz_steps(12) == 9
 assert collatz_steps(1000000) == 152
