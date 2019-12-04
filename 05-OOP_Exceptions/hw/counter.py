8e531a34c862a313544c6fb35950538a6470d333
Andrey <leksus@leksus.info>
2019-12-04 21:23:58 +0300

    05-OOP counter ready

--- 05-OOP_Exceptions/hw/counter.py
+++ 05-OOP_Exceptions/hw/counter.py
@@ -11,7 +11,23 @@ reset_instances_counter - сбросить счетчик экземпляров
 
 
 def instances_counter(cls):
-    """Some code"""
+    setattr(cls, 'counter', 0)
+
+    def __init__(self):
+        cls.counter += 1
+        cls.__init__
+
+    def get_created_instances(self=None):
+        return cls.counter
+
+    def reset_instances_counter(self=None):
+        total_instances = cls.counter
+        cls.counter = 0
+        return total_instances
+
+    setattr(cls, '__init__', __init__)
+    setattr(cls, 'get_created_instances', get_created_instances)
+    setattr(cls, 'reset_instances_counter', reset_instances_counter)
     return cls
 
 
