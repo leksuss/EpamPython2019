4ad5e9708f825e5a0c4adca1720f39c6369610cb
Andrey <leksus@leksus.info>
2020-01-01 19:03:31 +0300

    06 advanced hw3 ready

--- 06-advanced-python/hw/task3.py
+++ 06-advanced-python/hw/task3.py
@@ -7,4 +7,17 @@ print("It's fine")
 
 
 class Suppressor:
-    pass
+    def __init__(self, error):
+        self.error = error
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, *args):
+        if args[0] == self.error:
+            return True
+
+
+with Suppressor(ZeroDivisionError):
+    print(1 / 0)
+print("It's fine")
